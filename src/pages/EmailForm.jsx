780649bc147ab1971a98299ba1822779fb905298
Bintang Kurniawan <bintang.kurniawannn.98@gmail.com>
2025-05-12 16:32:21 +0700

    style: responsive form email & success alert

--- src/pages/EmailForm.jsx
+++ src/pages/EmailForm.jsx
@@ -1,17 +1,18 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import emailjs from "emailjs-com";
 import { Icon } from "@iconify/react/dist/iconify.js";
 
 const EmailForm = () => {
   const form = useRef();
+  const [success, setSuccess] = useState(false);
 
   const sendEmail = (e) => {
     e.preventDefault();
 
     emailjs.sendForm("service_4m5tap6", "template_0neykyf", form.current, "wGQqOPYlmdmNFzPvV").then(
       (result) => {
-        alert("Pesan berhasil dikirim!");
         form.current.reset();
+        setSuccess(true);
       },
       (error) => {
         alert("Gagal mengirim pesan.");
@@ -21,48 +22,78 @@ const EmailForm = () => {
   };
 
   return (
-    <div className="h-screen overflow-y-scroll">
-      <form ref={form} onSubmit={sendEmail} className="lg:mx-[110px] lg:my-[68px] h-fit relative">
-        <img src="/devoria.png" alt="Devoria" className="absolute top-0 right-0 w-[362px] opacity-[12%] -z-10" />
-        <div className="flex flex-col gap-10">
-          <div className="flex gap-4 items-center cursor-pointer hover:bg-gray-100 px-4 py-2 rounded-2xl w-fit" onClick={() => window.history.back()}>
-            <Icon icon="mingcute:left-line" className="w-10 h-10" />
-            <p className="font-medium text-2xl">Kembali</p>
-          </div>
-          <div className="max-w-[1000px]">
-            <p className="font-semibold text-8xl ">Sampaikan Aspirasi dan Pesan Anda</p>
-          </div>
-        </div>
-        <div className="mt-16 flex flex-col gap-10">
-          <div className="flex flex-col gap-4">
-            <label>
-              <p className="font-bold text-2xl">Masukkan Email Anda</p>
-            </label>
-            <input type="email" className="border-1 border-[#979797] rounded-2xl px-6 py-4 lg:w-[800px]" name="from_email" placeholder="example@gmail.com" required />
-          </div>
+    <>
+      {success && (
+        <div className="h-screen overflow-y-scroll z-50">
+          <div className="absolute z-30 bg-white w-full h-screen">
+            <img src="/Vector.svg" alt="vector" className="absolute lg:-top-36 top-24 w-full -z-10" />
+            <p className="font-semibold lg:text-2xl text-[#AFAFAF] text-center lg:mt-[96px] mt-[80px]">KEEP STRUGGLE, STAY TOGETHER</p>
 
-          <div className="flex flex-col gap-4">
-            <label>
-              <p className="font-bold text-2xl">Masukkan Nama Anda</p>
-            </label>
-            <input type="text" className="border-1 border-[#979797] rounded-2xl px-6 py-4 lg:w-[800px]" placeholder="Giorno Giovanna" name="from_name" required />
-          </div>
-
-          <div className="flex flex-row items-end justify-between">
-            <div className="flex flex-col gap-4">
-              <label>
-                <p className="font-bold text-2xl">Ketik Pesan Disini</p>
-              </label>
-              <textarea name="message" className="border-1 border-[#979797] rounded-2xl px-6 py-4 lg:w-[800px] lg:max-h-[250px]" placeholder="Ketik aspirasimu disini" required />
+            <div className="flex items-center rounded-full w-fit bg-[#EAF6EB] lg:p-10 p-[14px] mx-auto mt-[50px] z-50">
+              <div className="flex items-center rounded-full bg-[#CCEDC9] w-fit lg:p-[30px] p-[10px]">
+                <Icon icon="icon-park-solid:check-one" className="lg:w-[150px] w-[70px] h-[70px] lg:h-[150px] text-[#28BA2D]" />
+              </div>
             </div>
 
-            <button type="submit" className="hover:cursor-pointer  px-6 py-2 rounded-2xl bg-black text-white">
+            <div className="mx-auto flex flex-col mt-10 lg:gap-[30px] gap-4 lg:w-auto w-[343px]">
+              <p className="font-semibold lg:text-5xl text-2xl text-black text-center">Pesanmu telah berhasil dikirim</p>
+              <p className="lg:text-2xl text-black text-center">Mohon tunggu balasan dari kami yaa!</p>
+            </div>
+
+            <div className="hover:cursor-pointer px-6 py-4 border border-black rounded-2xl lg:w-fit mx-auto mt-[18px]  w-[343px]" onClick={() => (window.location.href = "/")}>
+              <p className="font-semibold text-2xl text-center">Kembali ke Beranda</p>
+            </div>
+          </div>
+        </div>
+      )}
+
+      <div className="h-screen overflow-y-scroll relative">
+        <form ref={form} onSubmit={sendEmail} className="lg:mx-[110px] lg:my-[68px] my-6 mx-4 h-fit relative">
+          <img src="/devoria.png" alt="Devoria" className="absolute top-0 right-0 w-[362px] opacity-[12%] -z-10" />
+          <div className="flex flex-col lg:gap-10 gap-[85px]">
+            <div className="flex gap-4 items-center cursor-pointer hover:bg-gray-100 px-4 py-2 rounded-2xl w-fit" onClick={() => (window.location.href = "/contact")}>
+              <Icon icon="mingcute:left-line" className="lg:w-10 lg:h-10 w-6 h-6" />
+              <p className="font-medium lg:text-2xl text-base">Kembali</p>
+            </div>
+            <div className="max-w-[1000px]">
+              <p className="font-semibold lg:text-8xl text-[40px] lg:text-start text-center ">Sampaikan Aspirasi dan Pesan Anda</p>
+            </div>
+          </div>
+          <div className="mt-16 flex flex-col lg:gap-10 gap-4">
+            <div className="flex flex-col gap-4">
+              <label>
+                <p className="font-bold text-2xl">Masukkan Email Anda</p>
+              </label>
+              <input type="email" className="border-1 border-[#979797] rounded-2xl px-6 py-4 lg:w-[800px]" name="from_email" placeholder="example@gmail.com" required />
+            </div>
+
+            <div className="flex flex-col gap-4">
+              <label>
+                <p className="font-bold text-2xl">Masukkan Nama Anda</p>
+              </label>
+              <input type="text" className="border-1 border-[#979797] rounded-2xl px-6 py-4 lg:w-[800px]" placeholder="Giorno Giovanna" name="from_name" required />
+            </div>
+
+            <div className="lg:flex flex-row items-end lg:justify-between w-full">
+              <div className="flex flex-col gap-4">
+                <label>
+                  <p className="font-bold text-2xl">Ketik Pesan Disini</p>
+                </label>
+                <textarea name="message" className="border-1 border-[#979797] rounded-2xl px-6 py-4 lg:w-[800px] w-full lg:max-h-[250px]" placeholder="Ketik aspirasimu disini" required />
+              </div>
+
+              <button type="submit" className="hover:cursor-pointer lg:block hidden  px-6 py-2 rounded-2xl bg-black text-white">
+                Kirim
+              </button>
+            </div>
+
+            <button type="submit" className="lg:hidden block hover:cursor-pointer mt-6  px-6 py-2 rounded-2xl bg-black text-white">
               Kirim
             </button>
           </div>
-        </div>
-      </form>
-    </div>
+        </form>
+      </div>
+    </>
   );
 };
 
