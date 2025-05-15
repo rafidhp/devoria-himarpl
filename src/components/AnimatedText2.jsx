2260fc359d70cd2ec9281b3489fe32272bb93937
Bintang Kurniawan <bintang.kurniawannn.98@gmail.com>
2025-05-15 10:24:50 +0700

    style: animated text

---
+++ src/components/AnimatedText2.jsx
@@ -0,0 +1,32 @@
+import { motion } from "framer-motion";
+
+const container = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const wordAnimation = {
+  hidden: { opacity: 0, y: 20 },
+  show: { opacity: 1, y: 0 },
+};
+
+const AnimatedText2 = ({ text, className = "", style = {} }) => {
+  return (
+    <motion.div variants={container} initial="hidden" animate="show">
+      <motion.h1 className={`${className}`} style={style}>
+        {text.split("").map((char, index) => (
+          <motion.span key={`char-${index}`} variants={wordAnimation}>
+            {char === " " ? "\u00A0" : char}
+          </motion.span>
+        ))}
+      </motion.h1>
+    </motion.div>
+  );
+};
+
+export default AnimatedText2;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,10 @@ import "swiper/css/pagination";
 import { Autoplay, Navigation, Pagination } from "swiper/modules";
 import Marquee from "react-fast-marquee";
 import { Contacts } from "./Contacts";
-
+import AnimatedText from "../components/AnimatedText";
+import AnimatedLongText from "../components/AnimatedLongText";
+import AnimatedText2 from "../components/AnimatedText2";
+import AnimatedLongText2 from "../components/AnimatedLongText2";
 export default function Home() {
   const textScrollDown = {
     writingMode: "vertical-rl",
@@ -30,16 +33,22 @@ export default function Home() {
         <div className="relative">
           <div className="flex lg:flex-row flex-col lg:gap-32 gap-4 lg:items-center items-start  lg:w-[1220px] lg:mx-[110px] mx-4">
             <div className="lg:max-w-[555px] flex flex-col gap-2">
-              <p className="font-bold text-[40px]/[64px] lg:text-[56px]/[64px]">HIMARPL</p>
+              {/* <p className="font-bold text-[40px]/[64px] lg:text-[56px]/[64px]">HIMARPL</p> */}
+              <AnimatedText text="HIMARPL" className="font-bold text-[40px]/[64px] lg:text-[56px]/[64px]" />
               <div className="bg-black p-2">
-                <p className="font-bold text-[32px] lg:text-[56px]/[64px] text-white underline decoration-[#FFE867] decoration-2 underline-offset-8">Kabinet Devoria</p>
+                {/* <p className="font-bold text-[32px] lg:text-[56px]/[64px] text-white underline decoration-[#FFE867] decoration-2 underline-offset-8">Kabinet Devoria</p> */}
+                <AnimatedText text="Kabinet Devoria" className="font-bold text-[32px] lg:text-[56px]/[64px] text-white underline decoration-[#FFE867] decoration-2 underline-offset-8" />
               </div>
             </div>
 
             <div className="lg:w-[580px] w-[343px]">
-              <p className="text-base/[24px]">
+              <AnimatedLongText
+                text="<b>Kabinet Devoria</b> adalah struktur kepengurusan Himpunan Mahasiswa Rekayasa Perangkat Lunak (HIMARPL) Universitas Pendidikan Indonesia (UPI) Kampus Cibiru untuk periode tahun 2025."
+                className="text-base/[24px]"
+              />
+              {/* <p className="text-base/[24px]">
                 <b>Kabinet Devoria</b> adalah struktur kepengurusan Himpunan Mahasiswa Rekayasa Perangkat Lunak (HIMARPL) Universitas Pendidikan Indonesia (UPI) Kampus Cibiru untuk periode tahun 2025.
-              </p>
+              </p> */}
             </div>
           </div>
 
@@ -47,12 +56,14 @@ export default function Home() {
             <img src="/fullteam.jpeg" alt="Kabinet Devoria" loading="lazy" className="lg:w-[1220px] lg:mx-[110px] mx-4 w-[343px] rounded-3xl z-10" />
 
             <img src="/HIMARPL.svg" alt="HIMARPL" className="lg:w-[1210px] w-[313px] absolute lg:top-[550px] lg:left-[130px] xl:left-[120px] top-[157px] left-[33px] z-20" />
-            <p style={textScrollDown} className="absolute lg:text-[16px] text-[8px] lg:tracking-[28px] tracking-[8px] top-0 xl:left-[84px] lg:left-11 left-0 font-bold">
+            <AnimatedText style={textScrollDown} className="absolute lg:text-[16px] text-[8px] lg:tracking-[28px] tracking-[8px] top-0 xl:left-[84px] lg:left-11 left-0 font-bold" text="scrolldown" />
+            {/* <p style={textScrollDown} className="absolute lg:text-[16px] text-[8px] lg:tracking-[28px] tracking-[8px] top-0 xl:left-[84px] lg:left-11 left-0 font-bold">
               scrolldown
-            </p>
-            <p style={textScrollDown} className="absolute lg:text-[16px] text-[8px] lg:tracking-[28px] lg:-bottom-[28px] xl:right-[84px] tracking-[8px] bottom-0 lg:right-11 right-0 font-bold">
+            </p> */}
+            <AnimatedText style={textScrollDown} className="absolute lg:text-[16px] text-[8px] lg:tracking-[28px] lg:-bottom-[28px] xl:right-[84px] tracking-[8px] bottom-0 lg:right-11 right-0 font-bold" text="scrolldown" />
+            {/* <p style={textScrollDown} className="absolute lg:text-[16px] text-[8px] lg:tracking-[28px] lg:-bottom-[28px] xl:right-[84px] tracking-[8px] bottom-0 lg:right-11 right-0 font-bold">
               scrolldown
-            </p>
+            </p> */}
           </div>
         </div>
       </div>
@@ -178,7 +189,8 @@ function Sponsor() {
   return (
     <div id="sponsor" className="bg-[#FFE867] w-full z-20  py-[20px]">
       <div className=" flex flex-col justify-center items-center">
-        <h4 className="text-[#806D00] text-2xl">Sponsor dan Mitra Kami</h4>
+        <AnimatedText2 text="Sponsor dan Mitra Kami" className="text-[#806D00] text-2xl" />
+        {/* <h4 className="text-[#806D00] text-2xl">Sponsor dan Mitra Kami</h4> */}
 
         <div className="flex items-center flex-row lg:gap-[54px] gap-4 h-auto justify-between xl:w-[1220px] lg:w-[1255px] w-[327px] mt-4">
           {sponsors.map((sponsor, index) => (
@@ -213,12 +225,18 @@ function Dedication() {
 
   return (
     <div id="dedikasi" className="bg-[#10316B]  w-full lg:pt-[123px] pt-[68px] z-10 relative flex flex-col justify-center items-center dedikasi lg:pb-[96px] pb-[84px]">
-      <img src="/Vector 67.svg" alt="vector" className="lg:w-[237px] w-[190px] absolute -top-[90px] right-0" />
+      <img src="/Vector 67.svg" alt="vector" className="lg:w-[237px] w-[190px] absolute -top-[90px] right-0 -z-10" />
       <img src="/Group 245.svg" alt="pixel" className="lg:w-[672px] w-[300px] absolute top-[280px] -z-10" />
       <img src="/Vector 50.svg" alt="vector" className="lg:w-[306px] w-[250px] absolute bottom-0 left-0 -z-10" />
       <div className="flex flex-col justify-center items-center lg:gap-8 gap-6 lg:w-[619px] w-[343px]">
-        <h1 className="lg:text-8xl/[54px] text-white text-center text-[64px]/[72px]">Dedikasi Kami</h1>
-        <p className="text-base/[24px] text-white text-center">Di bawah naungan Kabinet Devoria, HIMARPL UPI berkomitmen untuk menjadi wadah terbaik dalam mengembangkan potensi akademik, profesional, dan sosial seluruh anggota.</p>
+        <AnimatedLongText2 text="Dedikasi Kami" className="lg:text-8xl/[54px] text-white text-center text-[64px]/[72px] z-10" />
+        {/* <h1 className="lg:text-8xl/[54px] text-white text-center text-[64px]/[72px]">Dedikasi Kami</h1> */}
+
+        <AnimatedLongText
+          className="text-base/[24px] text-white text-center"
+          text="Di bawah naungan Kabinet Devoria, HIMARPL UPI berkomitmen untuk menjadi wadah terbaik dalam mengembangkan potensi akademik, profesional, dan sosial seluruh anggota."
+        />
+        {/* <p className="text-base/[24px] text-white text-center">Di bawah naungan Kabinet Devoria, HIMARPL UPI berkomitmen untuk menjadi wadah terbaik dalam mengembangkan potensi akademik, profesional, dan sosial seluruh anggota.</p> */}
       </div>
 
       <div className="lg:flex hidden flex-row items-center lg:gap-10 xl:gap-4 justify-between lg:w-[1280px] xl:w-[1220px] lg:mt-[103px] mt-8 z-10 overflow-x-scroll">
@@ -271,13 +289,17 @@ function Berita() {
         <div className="flex flex-col justify-center lg:w-[580px] w-[343px]">
           <img src="/Group 241.svg" alt="pixel" className="absolute max-w-[72px] lg:top-0 top-40 right-0" />
           <img src="/Group 252.svg" alt="pixel" className="absolute max-w-[303px] bottom-0 right-5 " />
-          <p className="lg:text-[64px]/[72px] text-[40px] min-w-[343px] font-semibold">
-            Akses Berita <br /> Aktual RPL <br />
-            di Sini.
-          </p>
-          <p className="mt-6 lg:mb-10 min-w-[343px]">
+
+          <AnimatedLongText className="lg:text-[64px]/[72px] text-[40px] !min-w-[343px] font-semibold" text="Akses Berita Aktual RPL di Sini." />
+
+          {/* <p className="lg:text-[64px]/[72px] text-[40px] min-w-[343px] font-semibold">Akses Berita Aktual RPL di Sini.</p> */}
+          <AnimatedLongText
+            className="mt-6 lg:mb-10 min-w-[343px]"
+            text="<b>Kabinet Devoria</b> adalah struktur kepengurusan Himpunan Mahasiswa Rekayasa Perangkat Lunak (HIMARPL) Universitas Pendidikan Indonesia (UPI) Kampus Cibiru untuk periode tahun 2025."
+          />
+          {/* <p className="mt-6 lg:mb-10 min-w-[343px]">
             <b> Kabinet Devoria</b> adalah struktur kepengurusan Himpunan Mahasiswa Rekayasa Perangkat Lunak (HIMARPL) Universitas Pendidikan Indonesia (UPI) Kampus Cibiru untuk periode tahun 2025.
-          </p>
+          </p> */}
           <div className="border w-[206px] h-14 lg:flex hidden justify-center items-center py-4 px-6 cursor-pointer rounded-[64px] ">
             <Link to="/berita" className=" text-[16px]/[24px] ">
               <p className="bg-transparent w-[206px] h-14 py-4 px-6 rounded-[64px]">Lihat Semua Berita</p>
@@ -297,7 +319,8 @@ function VisiMisi() {
 
       <div className="flex flex-col lg:rounded-3xl pb-4 rounded-lg lg:w-[1220px] w-[343px] gap-4 lg:h-[300px]  lg:border-8 border-2 border-[#FF4B4B]">
         <div className="flex flex-row items-center justify-between bg-[#FF4B4B] lg:gap-96 lg:rounded-b-3xl rounded-b-lg rounded-t-[4px] max-w-full lg:px-6 lg:py-4 px-4 py-2">
-          <h4 className="text-white lg:text-[40px] text-base">Visi</h4>
+          <AnimatedText2 className="text-white lg:text-[40px] text-base" text="Visi" />
+          {/* <h4 className="text-white lg:text-[40px] text-base">Visi</h4> */}
 
           <div className="flex flex-row items-center justify-between lg:gap-4 gap-[4.5px]">
             <div className="lg:w-6 lg:h-6 w-[6.75px] h-[6.75px] bg-[#B50000] rounded-full"></div>
@@ -306,13 +329,18 @@ function VisiMisi() {
           </div>
         </div>
 
-        <p className="text-white lg:text-[24px]/[40px] lg:mx-6 mx-4 text-base/[24px]">
+        <AnimatedLongText
+          className="text-white lg:text-[24px]/[40px] lg:mx-6 mx-4 text-base/[24px]"
+          text="Menjadi himpunan mahasiswa yang aktif, inovatif, dan kolaboratif dalam mengembangkan potensi akademik, profesional, dan sosial mahasiswa Rekayasa Perangkat Lunak UPI, serta berkontribusi positif bagi masyarakat dan dunia industri."
+        />
+        {/* <p className="text-white lg:text-[24px]/[40px] lg:mx-6 mx-4 text-base/[24px]">
           Menjadi himpunan mahasiswa yang aktif, inovatif, dan kolaboratif dalam mengembangkan potensi akademik, profesional, dan sosial mahasiswa Rekayasa Perangkat Lunak UPI, serta berkontribusi positif bagi masyarakat dan dunia industri.
-        </p>
+        </p> */}
       </div>
       <div className="flex flex-col lg:rounded-3xl rounded-lg lg:w-[1220px] w-[343px] gap-4 lg:h-[460px] lg:pb-5 pb-4 lg:border-8 border-2 border-[#5573FF] lg:mb-[96px] mb-10 bg-[#10316B]">
         <div className="flex flex-row items-center justify-between bg-[#5573FF] lg:gap-96 lg:rounded-b-3xl rounded-b-lg rounded-t-[4px] max-w-full lg:px-6 lg:py-4 px-4 py-2">
-          <h4 className="text-white lg:text-[40px] text-base">Misi</h4>
+          {/* <h4 className="text-white lg:text-[40px] text-base">Misi</h4> */}
+          <AnimatedText2 className="text-white lg:text-[40px] text-base" text="Misi" />
 
           <div className="flex flex-row items-center justify-between lg:gap-4 gap-[4.5px]">
             <div className="lg:w-6 lg:h-6 w-[6.75px] h-[6.75px] bg-[#001DA7] rounded-full"></div>
@@ -322,21 +350,15 @@ function VisiMisi() {
         </div>
 
         <ul>
-          <li>
-            <p className="text-white lg:text-[24px]/[40px] text-base/[40px] mx-6">• Menyelenggarakan program kerja yang mendukung pengembangan kompetensi teknis dan soft skills mahasiswa.</p>
-          </li>
-          <li>
-            <p className="text-white lg:text-[24px]/[40px] text-base/[40px] mx-6">• Membangun budaya organisasi yang profesional, adaptif, dan berintegritas.</p>
-          </li>
-          <li>
-            <p className="text-white lg:text-[24px]/[40px] text-base/[40px] mx-6">• Menjadi wadah aspirasi dan advokasi bagi mahasiswa Rekayasa Perangkat Lunak.</p>
-          </li>
-          <li>
-            <p className="text-white lg:text-[24px]/[40px] text-base/[40px] mx-6">• Meningkatkan kolaborasi dengan berbagai pihak, termasuk institusi pendidikan, industri, dan masyarakat.</p>
-          </li>
-          <li>
-            <p className="text-white lg:text-[24px]/[40px] text-base/[40px] mx-6">• Melaksanakan kegiatan sosial dan pengabdian kepada masyarakat yang relevan dengan bidang keilmuan.</p>
-          </li>
+          {[
+            "• Menyelenggarakan program kerja yang mendukung pengembangan kompetensi teknis dan soft skills mahasiswa.",
+            "• Membangun budaya organisasi yang profesional, adaptif, dan berintegritas.",
+            "• Menjadi wadah aspirasi dan advokasi bagi mahasiswa Rekayasa Perangkat Lunak.",
+            "• Meningkatkan kolaborasi dengan berbagai pihak, termasuk institusi pendidikan, industri, dan masyarakat.",
+            "• Melaksanakan kegiatan sosial dan pengabdian kepada masyarakat yang relevan dengan bidang keilmuan.",
+          ].map((i, idx) => (
+            <AnimatedLongText key={idx} className="text-white lg:text-[24px]/[40px] text-base/[40px] mx-6" text={i} />
+          ))}
         </ul>
       </div>
     </div>
@@ -358,7 +380,8 @@ function Sejarah() {
   ];
   return (
     <div id="journey" className="bg-[#10316B] w-full  items-center lg:gap-[135px] gap-[88px] flex flex-col justify-center lg:py-[68px] pt-[40px] pb-8">
-      <h1 className="text-white lg:text-8xl text-[64px]/[72px] text-center">Perjalanan HIMARPL</h1>
+      <AnimatedLongText2 className="text-white lg:text-8xl text-[64px]/[72px] text-center" text="Perjalanan HIMARPL" />
+      {/* <h1 className="text-white lg:text-8xl text-[64px]/[72px] text-center">Perjalanan HIMARPL</h1> */}
 
       <div className="w-full h-[600px] flex flex-col items-center justify-center relative bg-[#0B409C] gap-14">
         <img src="/Group 253.svg" alt="pixel" className="absolute lg:w-[128px] lg:-top-28 -top-11 w-[88px] left-0" />
