825edb05ca652afee7acc7db4f7e1115886c234f
Bintang Kurniawan <bintang.kurniawannn.98@gmail.com>
2025-07-13 18:24:37 +0700

    kalo gini bisa ga

--- src/components/Transition.jsx
+++ src/components/Transition.jsx
@@ -10,7 +10,6 @@ export default function Transition() {
           className="fixed top-0 bottom-0 right-full w-screen h-screen z-[100] bg-[#0B409C]"
           initial={{ x: "100%", width: "100%" }}
           animate={{ x: "0%", width: "0%" }}
-          exit={{ x: ["0%", "100%"], width: ["0%", "100%"] }}
           transition={{ duration: 0.8, ease: "easeInOut" }}
         />
 
